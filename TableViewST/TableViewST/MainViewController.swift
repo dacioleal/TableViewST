f3360951247f58a5b4bcca2e23a1e50368ff6aec
dacioleal <dacioleal@gmail.com>
2015-06-27 11:40:23 +0200

    Inicio de proyecto

---
+++ TableViewST/TableViewST/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  TableViewST
+//
+//  Created by Dacio Leal Rodriguez on 27/6/15.
+//  Copyright (c) 2015 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func loadView() {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+   
+}
