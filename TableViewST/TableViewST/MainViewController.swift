f4ea7f0c3a77298be43a7a1d8df0f1e8e38d5f45
dacioleal <dacioleal@gmail.com>
2015-06-28 12:02:12 +0200

    TableView datasource and delegate methods implementation

--- TableViewST/TableViewST/MainViewController.swift
+++ TableViewST/TableViewST/MainViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     private var tableView : UITableView!
     
+    private let stringsArray : [String] = ["primera", "segunda", "tercera"]
     
     override func loadView() {
         
         let backView : UIView = UIView(frame: UIScreen.mainScreen().bounds)
         backView.backgroundColor = UIColor.darkGrayColor()
         
-        self.tableView = UITableView(frame: CGRectMake(20.0, 44.0, 330.0, 500.0))
-        backView.addSubview(self.tableView)
+        tableView = UITableView(frame: CGRectMake(20.0, 44.0, 330.0, 500.0))
+        backView.addSubview(tableView)
         
         self.view = backView;
     }
@@ -27,15 +29,37 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        let sections = 2
+        
+        return sections
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stringsArray.count
+    }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let cellIdentifier = "myCell"
+        
+        return cell
+    }
    
 }
+
+
+
+
+
+
+
+
+
+
+
